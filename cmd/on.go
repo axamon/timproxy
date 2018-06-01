29257b66c8db74f379342b68aba4c92713a5a285
Alberto Bregliano <alberto.bregliano@telecomitalia.it>
2018-06-01 18:42:53 +0200

    ok

--- cmd/on.go
+++ cmd/on.go
@@ -28,13 +28,8 @@ import (
 // onCmd represents the on command
 var onCmd = &cobra.Command{
 	Use:   "on",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Attiva il proxy",
+	Long:  `Attiva il proxy selezionato in una nuova shell`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		viper.AddConfigPath(".")
@@ -54,6 +49,15 @@ to quickly create a Cobra application.`,
 		httpproxy := "http://" + matricola + ":" + passwordaziendale + host
 		httpsproxy := "https://" + matricola + ":" + passwordaziendale + host
 
+		os.Setenv("HTTP_PROXY", httpproxy)
+		os.Setenv("HTTPS_PROXY", httpsproxy)
+
+		cmdshell := exec.Command(cmdpath, "/C", "start", cmdpath)
+		errshell := cmdshell.Start()
+		if errshell != nil {
+			fmt.Fprintln(os.Stderr, errshell.Error())
+		}
+
 		fmt.Println(httpproxy, httpsproxy)
 		//fmt.Println("on called")
 	},
@@ -67,6 +71,11 @@ func init() {
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
 	// onCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// onCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
